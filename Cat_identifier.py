c3d4409402d98608b9a00e95ad13a931088dd3a7
Nitesh Kumar Singh <niteshks247@gmail.com>
2019-10-14 21:43:28 +0530

    Update Cat_identifier.py

--- Cat_identifier.py
+++ Cat_identifier.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import scipy
 from PIL import Image
 from scipy import ndimage
-from dnn_app_utils_v3 import *
+from dnn_app_utils import *
 
 %matplotlib inline
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
